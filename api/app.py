9d7794dbf807a416648a26a6f71fa181ae7e5bb1
Sam Potter <spotter1642@gmail.com>
2021-07-17 15:48:45 -0500

    Add a sandbox image

--- api/app.py
+++ api/app.py
@@ -1,7 +1,33 @@
-import flask
+import io
 
-app = flask.Flask(__name__)
+from flask import Flask, send_file
+import numpy as np
+from PIL import Image
+
+SANDBOX_ID = 'sandbox'
+
+app = Flask(__name__)
 
 @app.route('/api/message')
 def index():
     return {"message": "Hello, World"}
+
+@app.route('/api/image/<id>')
+def get_image_by_id(image_id):
+    if image_id == SANDBOX_ID:
+        image = _make_test_image()
+        return _send_image(image)
+    return {}
+
+
+def _make_test_image():
+    np.random.seed(42)
+    arr = np.random.rand(300, 300)
+    return np.uint8(arr)
+
+
+def _send_image(image):
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
+    buffer.seek(0)
+    return send_file(buffer,mimetype='image/png')
