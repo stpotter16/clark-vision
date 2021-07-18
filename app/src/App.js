551991fa834e02e2ca19de8415202ee9b338475a
Sam Potter <spotter1642@gmail.com>
2021-07-18 15:27:35 -0500

    Remove everything but the images

--- app/src/App.js
+++ app/src/App.js
@@ -2,15 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [currentMessage, setCurrentMessage] = useState('Hi');
   const [currentImage, setCurrentImage] = useState(null);
   const [imageID, setImageID] = useState(0);
-
-  useEffect(() => {
-    fetch('/api/message').then(res => res.json()).then(data => {
-      setCurrentMessage(data.message);
-    })
-  }, []);
 
   useEffect(() => {
     setCurrentImage('/api/image/sandbox')
@@ -24,7 +17,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>The current message is {currentMessage}</p>
         <img src={currentImage} alt='CT'/>
         <NewImageButton imageID={imageID} onButtonClick={handleButtonClick}/>
       </header>
