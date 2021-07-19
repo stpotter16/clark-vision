52eaa2319ca81ccd29f46943bcbfc3edb5beed7f
Sam Potter <spotter1642@gmail.com>
2021-07-18 20:01:43 -0500

    Add a Canvas component

---
+++ app/src/Canvas.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from "react";
+import { useCanvas } from "./CanvasContext";
+
+const Canvas = () => {
+    const {
+        canvasRef,
+        prepareCanvas,
+        startDrawing,
+        finishDrawing,
+        draw,
+    } = useCanvas();
+
+    useEffect(() => {
+        prepareCanvas();
+    }, []);
+
+    return (
+        <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+        />
+    );
+}
+
+export default Canvas;
