f12d98239922087e4c1f5c9d87b306e58b4daf46
Sam Potter <spotter1642@gmail.com>
2021-07-18 20:41:31 -0500

    Move handler logic into canvas component

--- app/src/Canvas.js
+++ app/src/Canvas.js
@@ -1,11 +1,52 @@
-import React, {useEffect} from "react";
-import {useCanvas} from "./CanvasContext";
+import React, {useEffect, useRef, useState} from "react";
 
 const Canvas = () => {
-    const {
-        canvasRef,
-        prepareCanvas,
-    } = useCanvas();
+    const [isDrawing, setIsDrawing] = useState(false);
+    const canvasRef = useRef(null);
+    const contextRef = useRef(null);
+
+    const prepareCanvas = () => {
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth * 2;
+        canvas.height = window.innerHeight * 2;
+        canvas.style.width = `${window.innerWidth}px`;
+        canvas.style.height = `${window.innerHeight}px`;
+
+        const context = canvas.getContext('2d');
+        context.scale(2, 2);
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = 5;
+        contextRef.current = context;
+    };
+
+    const startDrawing = ({ nativeEvent }) => {
+        const { offsetX, offsetY } = nativeEvent;
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+        setIsDrawing(true);
+    };
+
+    const finishDrawing = () => {
+        contextRef.current.closePath();
+        setIsDrawing(false);
+    };
+
+    const draw = ({ nativeEvent }) => {
+        if (!isDrawing) {
+            return;
+        }
+        const { offsetX, offsetY } = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke();
+    };
+
+    const clearCanvas = () => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context.fillStyle = "white";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    };
 
     useEffect(() => {
         prepareCanvas();
@@ -13,6 +54,9 @@ const Canvas = () => {
 
     return (
         <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
         ref={canvasRef}
         />
     );
